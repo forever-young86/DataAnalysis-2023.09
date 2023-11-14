bda33dcc97e3526fff076ae9f6df5941ef2d8877
YOUNG HWA SHIN <gloryhwa@gmail.com>
2023-11-14 12:18:36 +0900

    11/14

--- 08.Prototype/bp/chatbot.py
+++ 08.Prototype/bp/chatbot.py
@@ -3,12 +3,9 @@ import json, os
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-import requests, base64
-from PIL import Image, ImageDraw, ImageFont
-import matplotlib.pyplot as plt
-""" import bardapi, openai
 import util.chatbot_util as cu
- """
+""" import bardapi, openai """
+
 
 chatbot_bp = Blueprint('chatbot_bp', __name__)
 
@@ -105,43 +102,15 @@ def ocr():
 @chatbot_bp.route('/yolo', methods=['GET','POST'])
 def yolo():
     if request.method == 'GET':
-        return render_template('chatbot/yolo_form.html', menu=menu)
+        return render_template('chatbot/yolo.html', menu=menu)
     else:
-        file_image = request.files['image']     # yolo_form.html 에서 name="image" 이므로
+        color = request.form['color']
+        linewidth = int(request.form['linewidth'])
+        fontsize = int(request.form['fontsize'])
+        print(f'color={color}, linewidth={linewidth}, fontsize={fontsize}')
+        file_image = request.files['image']     # yolo.html 에서 name="image"인 것을 가져옴
         img_file = os.path.join(current_app.static_folder, f'upload/{file_image.filename}')
         file_image.save(img_file)
-        img_type = img_file.split('.')[-1]
-        if img_type == 'jfif':
-            img_type = 'jpg'
 
-        with open(os.path.join(current_app.static_folder, 'keys/etriAiKey.txt')) as f:
-            accessKey = f.read()
-        with open(img_file, 'rb') as f: 
-            img_content = base64.b64encode(f.read()).decode("utf8")  
-        openApiURL = "http://aiopen.etri.re.kr:8000/ObjectDetect"
-        headers={"Content-Type": "application/json; charset=UTF-8","Authorization": accessKey}
-        requestJson = { "argument": { "type": img_type, "file": img_content } }
-        result = requests.post(
-            openApiURL, headers=headers, data=json.dumps(requestJson)
-        ).json()
-        obj_list = result['return_object']['data']
-
-        img = Image.open(img_file)
-        draw = ImageDraw.Draw(img)
-        size = img.width + img.height
-        font_size = 16 if size < 1600 else 32 if size < 3200 else 48
-        line_width = 1 if size < 1600 else 2 if size < 3200 else 3
-        font = ImageFont.truetype('malgun.ttf', font_size)    
-        for obj in obj_list:
-            name = obj['class']
-            x, y = int(obj['x']), int(obj['y'])
-            w, h = int(obj['width']), int(obj['height'])
-            draw.rectangle(((x,y),(x+w,y+h)), outline=(255,0,0), width=line_width)
-            draw.text((x+10, y+10), name, font=font, fill=(255,0,0))
-        savefile = os.path.join(current_app.static_folder, 'result/yolo.png')
-        plt.imshow(img)
-        plt.axis('off')
-        plt.savefig(savefile, format='png')
-        mtime = os.stat(savefile).st_mtime      # 마지막 시간을 modify 함
-
-        return render_template('chatbot/yolo_res.html', menu=menu, mtime=mtime)
+        mtime = cu.proc_yolo(current_app.static_folder, img_file, color, linewidth, fontsize)
+        return json.dumps(str(mtime))
